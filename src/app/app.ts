eaa2e84bb8a4e8ca581731dfa160f56797878318
longdo-nlg <long.do3@namlongvn.com>
2025-09-16 14:47:25 +0700

    final

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
-import { Component, computed, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  signal,
+  effect,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   protected readonly title = signal('exam-signal');
@@ -18,10 +25,28 @@ export class App {
   constructor() {
     // TODO: Create effect to save theme to localStorage
     // Use localStorage.setItem('theme', this.theme()) and console.log
+    effect(() => {
+      localStorage.setItem('theme', this.theme());
+      console.log(`Theme changed to: ${this.theme()}`);
+    });
     // TODO: Create effect to log user activity changes
     // Read both isLoggedIn() and username() signals and console.log the status
+    effect(() => {
+      const status = this.isLoggedIn() ? 'logged in' : 'logged out';
+      const user = this.username();
+      console.log(`User ${user} is currently ${status}.`);
+    });
     // TODO: Create effect with cleanup for timer
     // Use setInterval to log every 5 seconds, and onCleanup to clear the interval
+    effect((onCleanup) => {
+      const interval = setInterval(() => {
+        console.log('Timer tick - Current theme:', this.theme());
+      }, 5000);
+      onCleanup(() => {
+        clearInterval(interval);
+        console.log('Timer cleaned up');
+      });
+    });
   }
 
   toggleTheme() {
